53190da690d96b0d5c52cd4cef1c06702c25dc82
DavR13 <55868116+DavR13@users.noreply.github.com>
2021-11-10 13:44:56 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import pygame
 import math
 import numpy as np
 
+
 class Planet:
 
-    def __init__(self, mass, position, init_velocity, img_file, id):
+    def __init__(self, mass, position, init_velocity, img_file):
         self.mass = mass
         self.position = position
         self.current_velocity = np.array(init_velocity)
         self.img = pygame.image.load(img_file)
-        self.id = id
+        self.radius = self.img.get_width()
 
     def update_position(self, time_step):
         self.position += self.current_velocity * time_step
@@ -30,60 +31,79 @@ class Planet:
                 force_vector = np.array([(force_dir[0] * GRAV_CONSTANT * self.mass * planet.mass / sqr_dist),
                                          (force_dir[1] * GRAV_CONSTANT * self.mass * planet.mass / sqr_dist)])
                 accel_vector = np.array([force_vector[0]/self.mass, force_vector[1]/self.mass])
+                self.current_velocity += accel_vector * time_step
 
-                force = math.sqrt(force_vector[0]**2 + force_vector[1]**2)
-                acceleration = float(force / self.mass)
+    def is_collision(self, all_planets):
+        for planet in all_planets:
+            if planet != self:
+                delta_x = planet.position[0] - self.position[0]
+                delta_y = planet.position[1] - self.position[1]
 
-                print(self.id)
-                print("--------")
-                print("OV = " + str(self.current_velocity))
-                print("DELTA V = ", str(accel_vector*time_step))
+                sqr_dist = delta_x**2 + delta_y**2
+                distance = math.sqrt(sqr_dist)
 
-                self.current_velocity += accel_vector*time_step
+                if distance < min(planet.radius, self.radius):
+                    return True
+
+        return False
 
 
-                print("FORCE VECTOR = " + str(force_vector))
-                print("ACCEL VECTOR = " + str(accel_vector))
-                # print("FORCE = " + str(force))
-                print("CV = " + str(self.current_velocity))
-
-size = width, height = 1200, 900
+DISPLAY_SIZE = DISPLAY_WIDTH, DISPLAY_HEIGHT = 1200, 900
 BLACK = (0, 0, 0)
 GRAV_CONSTANT = 9.8
+TIME_STEP = 1
 
 pygame.init()
-screen = pygame.display.set_mode((size))
+screen = pygame.display.set_mode(DISPLAY_SIZE)
 pygame.display.set_caption('Solar System')
 clock = pygame.time.Clock()
 
-planets = set()
-bluePlanet = Planet(10000, [400, 350], [float(0), float(0)], 'planet_img/bluePlanet.png', 'BLUE')
-redPlanet = Planet(400, [200, 700], [float(0),float(0)], 'planet_img/redPlanet.png', 'RED')
-planets.add(bluePlanet)
-planets.add(redPlanet)
 
-def draw():
+def draw(planets):
+
     screen.fill(BLACK)
-
     for planet in planets:
-        planet.update_velocity(planets, time_step)
-        planet.update_position(time_step)
+        update(planets)
         screen.blit(planet.img, planet.position)
 
 
-time_step = 1
-quit = False
-while not quit:
+def update(planets):
 
-    draw()
+    for planet in planets:
+        planet.update_velocity(planets, TIME_STEP)
+        planet.update_position(TIME_STEP)
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT or event.type == pygame.WINDOWCLOSE:
-            quit = True
 
-    clock.tick(20)
+def game_loop():
 
-    pygame.display.update()
+    planets = set()
+    blue_planet = Planet(10000, [400, 350], [float(0), float(0)], 'planet_img/bluePlanet.png')
+    red_planet = Planet(400, [200, 700], [float(-7), float(-10)], 'planet_img/redPlanet.png')
+    planets.add(blue_planet)
+    planets.add(red_planet)
 
-pygame.display.quit()
-pygame.quit()
+    game_exit = False
+    while not game_exit:
+
+        draw(planets)
+
+        for planet in planets:
+            if planet.is_collision(planets):
+                print("IMPACT!")
+                game_exit = True
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_exit = True
+
+            if event.type == pygame.WINDOWCLOSE:
+                game_exit = True
+
+        clock.tick(15)
+        pygame.display.update()
+
+
+if __name__ == "__main__":
+    game_loop()
+    pygame.display.quit()
+    pygame.quit()
